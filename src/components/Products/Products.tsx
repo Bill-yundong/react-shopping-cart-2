bbb041c00455ebd79349034c3baef513db9264cf
Bill Wu <yundongsoftwareit@gmail.com>
2026-04-15 09:16:34 -0400

    引入产品展示相关的数据异常

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -8,6 +8,10 @@ interface IProps {
 }
 
 const Products = ({ products }: IProps) => {
+  if (products?.length > 0) {
+    throw new Error('Product list rendering failed');
+  }
+  
   return (
     <S.Container>
       {products?.map((p) => (
--- src/services/products.ts
+++ src/services/products.ts
@@ -4,17 +4,9 @@ import { IGetProductsResponse } from 'models';
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const getProducts = async () => {
-  let response: IGetProductsResponse;
-
-  if (isProduction) {
-    response = await axios.get(
-      'https://react-shopping-cart-67954.firebaseio.com/products.json'
-    );
-  } else {
-    response = require('static/json/products.json');
-  }
-
-  const { products } = response.data || [];
-
-  return products;
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error('Failed to load product data'));
+    }, 1000);
+  }) as Promise<any>;
 };
